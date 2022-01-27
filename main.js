0cec75e7810e3fafb2dcfe9a89807e86d0e6ded3
maestroware <83496883+maestroware@users.noreply.github.com>
2022-01-27 15:02:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-song = ""
-songa = ""
+homie = ""
+no = ""
+lws = 0
+rws = 0
+homiestat = ""
+nostat = ""
 
 function preload(){
-    song = loadSound("music.mp3")
-    songa = loadSound("music2.mp3")
+    homie = loadSound("music.mp3")
+    no = loadSound("music2.mp3")
 }
 
 function setup() {
     canvas = createCanvas(600, 500)
-    canvas.center()
+    canvas.position(450, 400)
 
     video = createCapture(VIDEO)
     video.hide()
@@ -19,12 +23,24 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 600, 500)
+    fill("#8c6117")
+    stroke("#8c6117")
+    nostat = songa.isPlaying()
+    homiestat = song.isPlaying()
+    if(lws > 0.2){
+        circle(lwx, lwy, 20)
+        homie.stop()
+        if(nostat = false){
+            no.play()
+            document.getElementById("song").innerHTML = "Welcome to Los Santos - Non-lyrical"
+        }
+    }
 }
 
 function play() {
-    songa.play()
-    song.setVolume(1)
-    song.rate(1)
+    no.play()
+    no.setVolume(1)
+    no.rate(1)
 }
 
 function gp(results) {
@@ -36,6 +52,7 @@ function gp(results) {
         rwx = results[0].pose.rightWrist.x
         rwy = results[0].pose.rightWrist.y
         console.log(rwx +" rwy:" + rwy)
+        lws = results[0].pose.keypoints[9].score
     }
 }
 
